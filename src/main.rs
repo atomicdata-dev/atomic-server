993430ecdde6de329e901755a29ef4fe692f5844
Joep Meindertsma <joep@argu.co>
2020-07-05 20:21:36 +0200

    ad3 parsing, init file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 use promptly::{prompt, prompt_opt};
-use std::collections::HashMap;
 use serde_json::de::from_str;
+use std::{collections::HashMap, error::Error, fs::File, io::BufReader, path::Path};
 
 struct Model {
-    required_fields: Vec<Property>,
-    recommended_fields: Vec<Property>,
+    requires: Vec<Property>,
+    recommends: Vec<Property>,
     /// Slug
     shortname: String,
     /// URL
-    identifier: String,
+    subject: String,
 }
 
 struct Property {
     data_type: String,
     shortname: String,
     identifier: String,
-}
-
-enum DataType {
-    Integer,
-    String,
-    File,
 }
 
 struct Instance {
@@ -49,29 +43,23 @@ type Resource = HashMap<String, String>;
 
 fn main() {
     // let mapping = read_mapping();
-    let model = get_model("https://example.com/Person".into());
+    let mut store: Store = HashMap::new();
+    read_store_from_file(&mut store);
+    let model = get_model("https://example.com/Person".into(), &mut store);
 
-    let mut created_instance = Instance { fields: Vec::new() };
+    let mut created_instance: Resource = HashMap::new();
 
-    let selected_model_url = model.identifier;
+    let selected_model_url = model.subject;
+    created_instance.insert(
+        "https://atomicdata.dev/properties/isA".into(),
+        selected_model_url.into(),
+    );
 
-    // Sets the Class of the Resource
-    let is_a = Tuple {
-        property: "https://atomicdata.dev/properties/isA".into(),
-        value: selected_model_url.into(),
-    };
-
-    created_instance.fields.push(is_a);
-
-    for field in model.required_fields {
-        let tuple = Tuple {
-            value: prompt(field.shortname).unwrap(),
-            property: field.identifier,
-        };
-        created_instance.fields.push(tuple);
+    for field in model.requires {
+        created_instance.insert(field.identifier, prompt(field.shortname).unwrap());
     }
 
-    for field in model.recommended_fields {
+    for field in model.recommends {
         let mut input: Option<String> = None;
         let msg = format!("{} (optional)", &field.shortname);
         match field.data_type.as_str() {
@@ -87,23 +75,12 @@ fn main() {
                     None => (),
                 }
             }
-            _ => {
-                panic!("Unknown datatype")
-            }
+            _ => panic!("Unknown datatype: {}", field.data_type),
         };
-        println!("input: {:?}", input);
-        match input {
-            Some(value) => {
-                let tuple = Tuple {
-                    value: value.clone(),
-                    property: field.identifier,
-                };
-                created_instance.fields.push(tuple);
-            }
-            None => {}
+        if let Some(i) = input {
+            created_instance.insert(field.identifier, i.clone());
         }
     }
-    println!("Fields: {:?}", &created_instance.fields);
 }
 
 // Classes
@@ -129,40 +106,37 @@ const INTEGER: &str = "https://atomicdata.dev/datatypes/integer";
 const RESOURCE_ARRAY: &str = "https://atomicdata.dev/datatypes/resourceArray";
 const BOOLEAN: &str = "https://atomicdata.dev/datatypes/boolean";
 
-fn get_model(subject: String) -> Model {
-    let mut store: Store = HashMap::new();
-
+fn get_model(subject: String, store: &mut Store) -> Model {
     // Create the "Name" Property
-    let mut name_prop_resource: Resource = HashMap::new();
-    name_prop_resource.insert(IS_A.into(), PROPERTY.into());
-    name_prop_resource.insert(SHORTNAME.into(), "name".into());
-    name_prop_resource.insert(DESCRIPTION.into(), "The name of the person".into());
-    name_prop_resource.insert(DATATYPE_PROP.into(), STRING.into());
-    store.insert("https://example.com/name".into(), name_prop_resource);
+    // let mut name_prop_resource: Resource = HashMap::new();
+    // name_prop_resource.insert(IS_A.into(), PROPERTY.into());
+    // name_prop_resource.insert(SHORTNAME.into(), "name".into());
+    // name_prop_resource.insert(DESCRIPTION.into(), "The name of the person".into());
+    // name_prop_resource.insert(DATATYPE_PROP.into(), STRING.into());
+    // store.insert("https://example.com/name".into(), name_prop_resource);
 
-    // Create the "Age" Property
-    let mut age_prop_resource: Resource = HashMap::new();
-    age_prop_resource.insert(IS_A.into(), PROPERTY.into());
-    age_prop_resource.insert(SHORTNAME.into(), "age".into());
-    age_prop_resource.insert(DESCRIPTION.into(), "The age of the person".into());
-    age_prop_resource.insert(DATATYPE_PROP.into(), INTEGER.into());
-    store.insert("https://example.com/age".into(), age_prop_resource);
+    // // Create the "Age" Property
+    // let mut age_prop_resource: Resource = HashMap::new();
+    // age_prop_resource.insert(IS_A.into(), PROPERTY.into());
+    // age_prop_resource.insert(SHORTNAME.into(), "age".into());
+    // age_prop_resource.insert(DESCRIPTION.into(), "The age of the person".into());
+    // age_prop_resource.insert(DATATYPE_PROP.into(), INTEGER.into());
+    // store.insert("https://example.com/age".into(), age_prop_resource);
 
-    // Create the "Person" Class
-    let mut person_class_resource: Resource = HashMap::new();
-    person_class_resource.insert(IS_A.into(), CLASS.into());
-    person_class_resource.insert(SHORTNAME.into(), "name".into());
-    person_class_resource.insert(DESCRIPTION.into(), "A human bean".into());
-    person_class_resource.insert(REQUIRES.into(), "[\"https://example.com/name\",\"https://example.com/age\"]".into());
-    person_class_resource.insert(RECOMMENDS.into(), "[\"https://example.com/age\"]".into());
-    store.insert("https://example.com/Person".into(), person_class_resource);
-
-    println!("{:?}", store.keys());
-    println!("{:?}", store.get("https://example.com/name"));
+    // // Create the "Person" Class
+    // let mut person_class_resource: Resource = HashMap::new();
+    // person_class_resource.insert(IS_A.into(), CLASS.into());
+    // person_class_resource.insert(SHORTNAME.into(), "name".into());
+    // person_class_resource.insert(DESCRIPTION.into(), "A human bean".into());
+    // person_class_resource.insert(REQUIRES.into(), "[\"https://example.com/name\"]".into());
+    // person_class_resource.insert(RECOMMENDS.into(), "[\"https://example.com/age\"]".into());
+    // store.insert("https://example.com/Person".into(), person_class_resource);
 
     // The string representation of the model
     let model_strings = store.get(&subject).expect("Model not found");
-    let shortname = model_strings.get(SHORTNAME).expect("Model has no shortname");
+    let shortname = model_strings
+        .get(SHORTNAME)
+        .expect("Model has no shortname");
     let requires_string = model_strings.get(REQUIRES).expect("No required props");
     let recommends_string = model_strings.get(RECOMMENDS).expect("No recommended props");
     let requires: Vec<Property> = get_properties(requires_string.into(), &store);
@@ -174,8 +148,14 @@ fn get_model(subject: String) -> Model {
         for prop_url in string_vec {
             let property_resource = store.get(&*prop_url).expect("Model not found");
             let property = Property {
-                data_type: property_resource.get(DATATYPE_PROP).expect("Datatype not found").into(),
-                shortname: property_resource.get(SHORTNAME).expect("Shortname not found").into(),
+                data_type: property_resource
+                    .get(DATATYPE_PROP)
+                    .expect("Datatype not found")
+                    .into(),
+                shortname: property_resource
+                    .get(SHORTNAME)
+                    .expect("Shortname not found")
+                    .into(),
                 identifier: prop_url.into(),
             };
             properties.push(property)
@@ -184,51 +164,58 @@ fn get_model(subject: String) -> Model {
     }
 
     let model = Model {
-        required_fields: requires,
-        recommended_fields: recommends,
+        requires,
+        recommends,
         shortname: shortname.into(),
-        identifier: subject,
+        subject,
     };
 
-    // let name_prop = Property {
-    //     data_type: STRING.into(),
-    //     shortname: "name".into(),
-    //     identifier: "https://example.com/name".into(),
-    // };
-    // let age_prop = Property {
-    //     data_type: INTEGER.into(),
-    //     shortname: "age".into(),
-    //     identifier: "https://example.com/age".into(),
-    // };
-    // let model = Model {
-    //     required_fields: vec![name_prop],
-    //     recommended_fields: vec![age_prop],
-    //     shortname: shortname.into(),
-    //     identifier: "https://example.com/Person".into(),
-    // };
-
-    // println!("shortname: {:?}", model.shortname);
-
-    // let name_prop = Property {
-    //     data_type: DataType::String,
-    //     shortname: "name".into(),
-    //     identifier: "https://example.com/name".into(),
-    // };
-    // let age_prop = Property {
-    //     data_type: DataType::Integer,
-    //     shortname: "age".into(),
-    //     identifier: "https://example.com/age".into(),
-    // };
-    // let model = Model {
-    //     required_fields: vec![name_prop],
-    //     recommended_fields: vec![age_prop],
-    //     shortname: "person".into(),
-    //     identifier: "https://example.com/Person".into(),
-    // };
     return model;
 }
 
 fn read_mapping() -> Vec<Mapping> {
     let mapping: Vec<Mapping> = Vec::new();
     return mapping;
+}
+
+/// Reads the .json graph from this directory
+fn read_store_from_file(store: &mut Store) -> &Store {
+    let store_path = Path::new("./init.ad3");
+
+    match std::fs::read_to_string(store_path) {
+        Ok(contents) => {
+            for line in contents.lines() {
+                match line.chars().next() {
+                    // These are comments
+                    Some('#') => {}
+                    Some(' ') => {}
+                    // That's an array, awesome
+                    Some('[') => {
+                        let string_vec: Vec<String> = from_str(line).unwrap();
+                        let subject = &string_vec[0];
+                        let property = &string_vec[1];
+                        let value = &string_vec[2];
+                        match &mut store.get_mut(&*subject) {
+                            Some(existing) => {
+                                existing.insert(property.into(), value.into());
+                            }
+                            None => {
+                                let mut resource: Resource = HashMap::new();
+                                resource.insert(property.into(), value.into());
+                                store.insert(subject.into(), resource);
+                            }
+                        }
+                    }
+                    Some(_) => {
+                        panic!("Parsing error")
+                    }
+                    None => {}
+                };
+            }
+            println!("Store: {:?}", store.keys())
+        }
+        Err(_) => panic!("Parsing error..."),
+    }
+
+    return store;
 }
