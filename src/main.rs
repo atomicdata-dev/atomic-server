8be972dac8bede80f31cfe5b14c82736eb1e7d45
Joep Meindertsma <joep@argu.co>
2020-07-05 23:21:45 +0200

    read + write from files!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use promptly::{prompt, prompt_opt};
+use regex::Regex;
 use serde_json::de::from_str;
-use std::{collections::HashMap, error::Error, fs::File, io::BufReader, path::Path};
+use std::{collections::HashMap, fs, path::Path};
+use uuid;
 
 struct Model {
     requires: Vec<Property>,
     recommends: Vec<Property>,
-    /// Slug
     shortname: String,
     /// URL
     subject: String,
@@ -17,23 +18,8 @@ struct Property {
     identifier: String,
 }
 
-struct Instance {
-    fields: Vec<Tuple>,
-}
-
-#[derive(Debug)]
-struct Tuple {
-    /// The URL of the Property
-    property: String,
-    /// The actual value, should not only be string but way more
-    value: String,
-}
-
 /// Maps shortanmes to URLs
-struct Mapping {
-    shortname: String,
-    url: String,
-}
+type Mapping = HashMap<String, String>;
 
 /// The in-memory store of data, containing the Resources, Properties and Classes
 type Store = HashMap<String, Resource>;
@@ -42,31 +28,33 @@ type Store = HashMap<String, Resource>;
 type Resource = HashMap<String, String>;
 
 fn main() {
-    // let mapping = read_mapping();
+    // Reads the shortname + URL map
+    let mut mapping = read_mapping_from_file();
+    println!("mapping: {:?}", mapping);
     let mut store: Store = HashMap::new();
+    // The store contains the classes and properties
     read_store_from_file(&mut store);
     let model = get_model("https://example.com/Person".into(), &mut store);
 
-    let mut created_instance: Resource = HashMap::new();
+    let mut new_resource: Resource = HashMap::new();
 
-    let selected_model_url = model.subject;
-    created_instance.insert(
+    new_resource.insert(
         "https://atomicdata.dev/properties/isA".into(),
-        selected_model_url.into(),
+        String::from(&model.subject),
     );
 
     for field in model.requires {
-        created_instance.insert(field.identifier, prompt(field.shortname).unwrap());
+        new_resource.insert(field.identifier, prompt(field.shortname).unwrap());
     }
 
     for field in model.recommends {
         let mut input: Option<String> = None;
         let msg = format!("{} (optional)", &field.shortname);
         match field.data_type.as_str() {
-            STRING => {
+            URL::STRING => {
                 input = prompt_opt(&msg).unwrap();
             }
-            INTEGER => {
+            URL::INTEGER => {
                 let number: Option<u32> = prompt_opt(&msg).unwrap();
                 match number {
                     Some(nr) => {
@@ -78,67 +66,60 @@ fn main() {
             _ => panic!("Unknown datatype: {}", field.data_type),
         };
         if let Some(i) = input {
-            created_instance.insert(field.identifier, i.clone());
+            new_resource.insert(field.identifier, i.clone());
         }
     }
+
+    let subject = format!("https://example.com/{}", uuid::Uuid::new_v4());
+    println!("Resource created with URL: {}", &subject);
+
+    prompt_bookmark(&mut mapping, &subject);
+
+    // Add created_instance to store
+    store.insert(subject, new_resource);
+    // Publish new resource to IPFS
+    // TODO!
+    // Save the store locally
+    write_store_to_disk(&store);
+    write_mapping_to_disk(&mapping);
 }
 
-// Classes
-const CLASS: &str = "https://atomicdata.dev/classes/Class";
-const PROPERTY: &str = "https://atomicdata.dev/classes/Property";
-const DATATYPE_CLASS: &str = "https://atomicdata.dev/classes/Datatype";
+pub mod URL {
+    // Classes
+    pub const CLASS: &str = "https://atomicdata.dev/classes/Class";
+    pub const PROPERTY: &str = "https://atomicdata.dev/classes/Property";
+    pub const DATATYPE_CLASS: &str = "https://atomicdata.dev/classes/Datatype";
 
-// Properties
-const SHORTNAME: &str = "https://atomicdata.dev/properties/shortname";
-const DESCRIPTION: &str = "https://atomicdata.dev/properties/description";
-// ... for Properties
-const IS_A: &str = "https://atomicdata.dev/properties/isA";
-const DATATYPE_PROP: &str = "https://atomicdata.dev/properties/datatype";
-// ... for Classes
-const REQUIRES: &str = "https://atomicdata.dev/properties/requires";
-const RECOMMENDS: &str = "https://atomicdata.dev/properties/recommends";
+    // Properties
+    pub const SHORTNAME: &str = "https://atomicdata.dev/properties/shortname";
+    pub const DESCRIPTION: &str = "https://atomicdata.dev/properties/description";
+    // ... for Properties
+    pub const IS_A: &str = "https://atomicdata.dev/properties/isA";
+    pub const DATATYPE_PROP: &str = "https://atomicdata.dev/properties/datatype";
+    // ... for Classes
+    pub const REQUIRES: &str = "https://atomicdata.dev/properties/requires";
+    pub const RECOMMENDS: &str = "https://atomicdata.dev/properties/recommends";
 
-// Datatypes
-const STRING: &str = "https://atomicdata.dev/datatypes/string";
-const SLUG: &str = "https://atomicdata.dev/datatypes/slug";
-const ATOMIC_URL: &str = "https://atomicdata.dev/datatypes/atomicURL";
-const INTEGER: &str = "https://atomicdata.dev/datatypes/integer";
-const RESOURCE_ARRAY: &str = "https://atomicdata.dev/datatypes/resourceArray";
-const BOOLEAN: &str = "https://atomicdata.dev/datatypes/boolean";
+    // Datatypes
+    pub const STRING: &str = "https://atomicdata.dev/datatypes/string";
+    pub const SLUG: &str = "https://atomicdata.dev/datatypes/slug";
+    pub const ATOMIC_URL: &str = "https://atomicdata.dev/datatypes/atomicURL";
+    pub const INTEGER: &str = "https://atomicdata.dev/datatypes/integer";
+    pub const RESOURCE_ARRAY: &str = "https://atomicdata.dev/datatypes/resourceArray";
+    pub const BOOLEAN: &str = "https://atomicdata.dev/datatypes/boolean";
+}
 
+/// Retrieves a model from the store by subject URL and converts it into a model useful for forms
 fn get_model(subject: String, store: &mut Store) -> Model {
-    // Create the "Name" Property
-    // let mut name_prop_resource: Resource = HashMap::new();
-    // name_prop_resource.insert(IS_A.into(), PROPERTY.into());
-    // name_prop_resource.insert(SHORTNAME.into(), "name".into());
-    // name_prop_resource.insert(DESCRIPTION.into(), "The name of the person".into());
-    // name_prop_resource.insert(DATATYPE_PROP.into(), STRING.into());
-    // store.insert("https://example.com/name".into(), name_prop_resource);
-
-    // // Create the "Age" Property
-    // let mut age_prop_resource: Resource = HashMap::new();
-    // age_prop_resource.insert(IS_A.into(), PROPERTY.into());
-    // age_prop_resource.insert(SHORTNAME.into(), "age".into());
-    // age_prop_resource.insert(DESCRIPTION.into(), "The age of the person".into());
-    // age_prop_resource.insert(DATATYPE_PROP.into(), INTEGER.into());
-    // store.insert("https://example.com/age".into(), age_prop_resource);
-
-    // // Create the "Person" Class
-    // let mut person_class_resource: Resource = HashMap::new();
-    // person_class_resource.insert(IS_A.into(), CLASS.into());
-    // person_class_resource.insert(SHORTNAME.into(), "name".into());
-    // person_class_resource.insert(DESCRIPTION.into(), "A human bean".into());
-    // person_class_resource.insert(REQUIRES.into(), "[\"https://example.com/name\"]".into());
-    // person_class_resource.insert(RECOMMENDS.into(), "[\"https://example.com/age\"]".into());
-    // store.insert("https://example.com/Person".into(), person_class_resource);
-
     // The string representation of the model
     let model_strings = store.get(&subject).expect("Model not found");
     let shortname = model_strings
-        .get(SHORTNAME)
+        .get(URL::SHORTNAME)
         .expect("Model has no shortname");
-    let requires_string = model_strings.get(REQUIRES).expect("No required props");
-    let recommends_string = model_strings.get(RECOMMENDS).expect("No recommended props");
+    let requires_string = model_strings.get(URL::REQUIRES).expect("No required props");
+    let recommends_string = model_strings
+        .get(URL::RECOMMENDS)
+        .expect("No recommended props");
     let requires: Vec<Property> = get_properties(requires_string.into(), &store);
     let recommends: Vec<Property> = get_properties(recommends_string.into(), &store);
 
@@ -149,11 +130,11 @@ fn get_model(subject: String, store: &mut Store) -> Model {
             let property_resource = store.get(&*prop_url).expect("Model not found");
             let property = Property {
                 data_type: property_resource
-                    .get(DATATYPE_PROP)
+                    .get(URL::DATATYPE_PROP)
                     .expect("Datatype not found")
                     .into(),
                 shortname: property_resource
-                    .get(SHORTNAME)
+                    .get(URL::SHORTNAME)
                     .expect("Shortname not found")
                     .into(),
                 identifier: prop_url.into(),
@@ -173,14 +154,42 @@ fn get_model(subject: String, store: &mut Store) -> Model {
     return model;
 }
 
-fn read_mapping() -> Vec<Mapping> {
-    let mapping: Vec<Mapping> = Vec::new();
+fn read_mapping_from_file() -> Mapping {
+    let mut mapping: Mapping = HashMap::new();
+    let mapping_path = Path::new("./default_mapping.amp");
+
+    match std::fs::read_to_string(mapping_path) {
+        Ok(contents) => {
+            for line in contents.lines() {
+                match line.chars().next() {
+                    Some('#') => {}
+                    Some(' ') => {}
+                    Some(_) => {
+                        let split: Vec<&str> = line.split("=").collect();
+                        if split.len() == 2 {
+                            &mapping.insert(String::from(split[0]), String::from(split[1]));
+                        } else {
+                            println!("Error reading line {:?} in {:?}", line, mapping_path);
+                        };
+                    }
+                    None => {}
+                };
+            }
+        }
+        Err(_) => panic!("error reading mapping file {:?}", mapping_path),
+    }
     return mapping;
 }
 
-/// Reads the .json graph from this directory
+/// Reads the .ad3 (Atomic Data Triples) graph from this directory
 fn read_store_from_file(store: &mut Store) -> &Store {
-    let store_path = Path::new("./init.ad3");
+    let default_store_path = Path::new("./default_store.ad3");
+    let user_store_path = Path::new("./user_store.ad3");
+    let mut store_path = default_store_path;
+
+    if user_store_path.exists() {
+        store_path = user_store_path;
+    }
 
     match std::fs::read_to_string(store_path) {
         Ok(contents) => {
@@ -191,7 +200,11 @@ fn read_store_from_file(store: &mut Store) -> &Store {
                     Some(' ') => {}
                     // That's an array, awesome
                     Some('[') => {
-                        let string_vec: Vec<String> = from_str(line).unwrap();
+                        let string_vec: Vec<String> =
+                            from_str(line).expect(&*format!("Parsing error in {:?}", store_path));
+                        if string_vec.len() != 3 {
+                            panic!(format!("Wrong length of array in {:?} at line {:?}: wrong length of array, should be 3", store_path, line))
+                        }
                         let subject = &string_vec[0];
                         let property = &string_vec[1];
                         let value = &string_vec[2];
@@ -206,16 +219,63 @@ fn read_store_from_file(store: &mut Store) -> &Store {
                             }
                         }
                     }
-                    Some(_) => {
-                        panic!("Parsing error")
-                    }
+                    Some(_) => println!("Parsing error in {:?} at {:?}", store_path, line),
                     None => {}
                 };
             }
-            println!("Store: {:?}", store.keys())
         }
         Err(_) => panic!("Parsing error..."),
     }
 
     return store;
+}
+
+fn write_store_to_disk(store: &Store) {
+    let user_store_path = "./user_store.ad3";
+    let mut file_string: String = String::new();
+    for (subject, resource) in store {
+        for (property, value) in resource {
+            // let ad3_atom = format!("[\"{}\",\"{}\",\"{}\"]\n", subject, property, value.replace("\"", "\\\""));
+            let mut ad3_atom =
+                serde_json::to_string(&vec![subject, property, value]).expect("Can't serialize");
+            ad3_atom.push_str("\n");
+            &file_string.push_str(&*ad3_atom);
+        }
+        // file_string.push(ch);
+    }
+    fs::write(user_store_path, file_string).expect("Unable to write file");
+}
+
+fn write_mapping_to_disk(mapping: &Mapping) {
+    let user_mapping_path = "./user_mapping.amp";
+    let mut file_string: String = String::new();
+    for (key, url) in mapping {
+        let map = format!("{}={}\n", key, url);
+        &file_string.push_str(&*map);
+    }
+    fs::write(user_mapping_path, file_string).expect("Unable to write file");
+}
+
+fn prompt_bookmark(mapping: &mut Mapping, subject: &String) {
+    let re = Regex::new(r"^[a-z0-9]+(?:-[a-z0-9]+)*$").unwrap();
+    let mut shortname: Option<String> = prompt_opt("Local Bookmark (optional)").unwrap();
+    loop {
+        match shortname {
+            Some(sn) => {
+                if re.is_match(&*sn) {
+                    &mut mapping.insert(sn, String::from(subject));
+                    break;
+                } else if mapping.contains_key(&*sn) {
+                    shortname =
+                        prompt_opt("You're already using that shortname, try something else")
+                            .unwrap();
+                } else {
+                    shortname =
+                        prompt_opt("Not a valid bookmark, only use letters, numbers, and '-'")
+                            .unwrap();
+                }
+            }
+            None => {}
+        }
+    }
 }
