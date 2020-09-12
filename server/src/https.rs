6c05d70cef847de2416df5aa3614bbb56ae3a0f3
Joep Meindertsma <joep@argu.co>
2020-09-12 17:56:48 +0200

    keys

--- server/src/https.rs
+++ server/src/https.rs
@@ -138,7 +138,7 @@ pub fn get_ssl_config(config: &crate::config::Config) -> Result<rustls::ServerCo
     let cert_chain = certs(cert_file).unwrap();
     log::info!("Opening private keys...");
     let mut keys = rsa_private_keys(key_file).unwrap();
-    log::info!("Setting certs...");
+    log::info!("Setting certs... {:?}", keys);
     ssl_config
         .set_single_cert(cert_chain, keys.remove(0))
         .unwrap();
