adf514a5a537ba017a79b5f55668c012b26b39af
Joep Meindertsma <joep@argu.co>
2020-11-22 17:53:05 +0100

    add remove function

--- cli/src/commit.rs
+++ cli/src/commit.rs
@@ -1,22 +1,39 @@
 use crate::{Context, delta::argument_to_url};
 use atomic_lib::{errors::AtomicResult};
 
-/// Perfoprm a Commit using the Set method - create or update a resource
-pub fn set(context: &mut Context) -> AtomicResult<()> {
-    let subcommand_matches = context.matches.subcommand_matches("set").unwrap();
-    let subject = argument_to_url(context, "set", "subject")?;
-    let prop = argument_to_url(context, "set", "property")?;
+/// Apply a Commit using the Set method - create or update a value in a resource
+pub fn set(context: &Context) -> AtomicResult<()> {
+    let subcommand = "set";
+    let subcommand_matches = context.matches.subcommand_matches(subcommand).clone().unwrap();
+    let subject = argument_to_url(context, subcommand, "subject")?;
+    let prop = argument_to_url(context, subcommand, "property")?;
     let val = subcommand_matches.value_of("value").unwrap();
-
-    // let resource = context.store.get_resource_extended(&subject)?;
-    // resource.set_by_shortname(&property, &value)?;
-    // resource.save();
-    // let signer = context.author_subject;
-    let write_ctx = context.get_write_context();
-    let mut commit_builder = atomic_lib::commit::CommitBuilder::new(subject, write_ctx.author_subject);
+    let mut commit_builder = builder(context, subject);
     commit_builder.set(prop, val.into());
+    post(context, commit_builder)?;
+    Ok(())
+}
+
+/// Apply a Commit using the Remove method - removes a property from a resource
+pub fn remove(context: &Context) -> AtomicResult<()> {
+    let subcommand = "remove";
+    let subject = argument_to_url(context, subcommand, "subject")?;
+    let prop = argument_to_url(context, subcommand, "property")?;
+    let mut commit_builder = builder(context, subject);
+    commit_builder.remove(prop);
+    post(context, commit_builder)?;
+    Ok(())
+}
+
+fn builder(context: &Context, subject: String) -> atomic_lib::commit::CommitBuilder {
+    let write_ctx = context.get_write_context();
+    atomic_lib::commit::CommitBuilder::new(subject, write_ctx.author_subject)
+}
+
+/// Posts the Commit and applies it to the server
+fn post(context: &Context , commit_builder: atomic_lib::commit::CommitBuilder) -> AtomicResult<()> {
+    let write_ctx = context.get_write_context();
     let commit = commit_builder.sign(&write_ctx.author_private_key)?;
-    println!("{:?}", commit);
     atomic_lib::client::post_commit(&format!("{}commit", &write_ctx.base_url), &commit)?;
     Ok(())
 }
