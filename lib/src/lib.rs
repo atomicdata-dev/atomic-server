87aca64c7e57864a0c3023551b00bcbf44924705
Joep Meindertsma <joep@argu.co>
2021-01-03 22:24:45 +0100

    Fix test. All green

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -9,7 +9,7 @@ The [Store](struct.Store) contains most of the logic that you need.
 // Import the `Storelike` trait to get access to most functions
 use atomic_lib::Storelike;
 // Start with initializing the in-memory store
-let store = atomic_lib::Store::init();
+let store = atomic_lib::Store::init().unwrap();
 // Pre-load the default Atomic Data Atoms (from atomicdata.dev),
 // this is not necessary, but will probably make your project a bit faster
 store.populate().unwrap();
@@ -46,13 +46,13 @@ let subject = new_property.get_subject().clone();
 // Because these are signed, we need an Agent, which has a private key to sign Commits.
 let agent = store.create_agent("my_agent").unwrap();
 store.set_default_agent(agent);
-store.commit_resource_changes_locally(&mut new_property).unwrap_err();
+new_property.save(&store).unwrap_err();
 // But.. when we commit, we get an error!
 // Because we haven't set all the properties required for the Property class.
 // We still need to set `shortname` and `datatype`.
 new_property.set_propval_by_shortname("shortname", "age", &store).unwrap();
 new_property.set_propval_by_shortname("datatype", atomic_lib::urls::INTEGER, &store).unwrap();
-store.commit_resource_changes_locally(&mut new_property).unwrap();
+new_property.save(&store).unwrap();
 // Now the changes to the resource applied to the store, and we can fetch the newly created resource!
 let fetched_new_resource = store.get_resource(&subject).unwrap();
 assert!(fetched_new_resource.get_shortname("description", &store).unwrap().to_string() == "the age of a person");
