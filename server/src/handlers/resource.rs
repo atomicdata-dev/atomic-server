69695d90182cfd4155ba562a6042e716a5f2a1b9
Joep Meindertsma <joep@ontola.io>
2021-06-26 22:42:44 +0200

    #141 dynamic resources n-triples fix

--- server/src/handlers/resource.rs
+++ server/src/handlers/resource.rs
@@ -70,7 +70,7 @@ pub async fn get_resource(
             Ok(builder.body(body))
         }
         ContentType::TURTLE | ContentType::NT => {
-            let atoms = store.get_resource(&subject)?.to_atoms()?;
+            let atoms = resource.to_atoms()?;
             let body = atomic_lib::serialize::atoms_to_ntriples(atoms, store)?;
             Ok(builder.body(body))
         }
