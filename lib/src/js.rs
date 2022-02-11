8c556e1c6a135dd7572eb542355cd1e1c9daf249
Joep Meindertsma <joep@ontola.io>
2022-02-11 15:56:23 +0100

    #73 wasmedge JS broken

--- lib/src/js.rs
+++ lib/src/js.rs
@@ -2,26 +2,36 @@
 
 #[cfg(test)]
 mod test {
-    use quick_js::{Context, JsValue};
+    fn wasm() {
+        use std::path::PathBuf;
 
-    #[test]
-    fn quick_js() {
-        let context = Context::new().unwrap();
+        // create a Loader context
+        let loader = Loader::create(None).expect("fail to create a Loader context");
 
-        // Eval.
-        let value = context.eval("1 + 2").unwrap();
-        assert_eq!(value, JsValue::Int(3));
-        let value = context
-            .eval_as::<String>(" var x = 100 + 250; x.toString() ")
-            .unwrap();
-        assert_eq!(&value, "350");
+        // load a wasm module from a specified wasm file, and return a WasmEdge AST Module instance
+        let path = PathBuf::from("fibonacci.wasm");
+        let mut module = loader
+            .from_file(path)
+            .expect("fail to load the WebAssembly file");
 
-        // Callbacks.
-        context
-            .add_callback("myCallback", |a: i32, b: i32| a + b)
-            .unwrap();
-        let callback_demo = include_str!("./js/callback_demo.js");
-        let callback_val = context.eval(callback_demo).unwrap();
-        assert_eq!(callback_val, JsValue::Int(30));
+        use wasmedge_sys::{Config, Store, Vm};
+
+        // create a Config context
+        let config = Config::create().expect("fail to create a Config context");
+
+        // create a Store context
+        let store = Store::create().expect("fail to create a Store context");
+
+        // create a Vm context with the given Config and Store
+        let vm = Vm::create(Some(&config), Some(&store)).expect("fail to create a Vm context");
+
+        use wasmedge_sys::Value;
+
+        // run a function
+        let returns = vm
+            .run_wasm_from_module(&mut module, "fib", [Value::from_i32(5)])
+            .expect("fail to run the target function in the module");
+
+        println!("The result of fib(5) is {}", returns[0].to_i32());
     }
 }
