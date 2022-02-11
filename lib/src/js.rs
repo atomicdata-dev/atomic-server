0df941517553a639d395a40dc3b9682e286a18b1
Joep Meindertsma <joep@ontola.io>
2022-02-11 11:04:23 +0100

    #73-quick-js-rs

--- lib/src/js.rs
+++ lib/src/js.rs
@@ -1,3 +1,5 @@
+//! QuickJS runtime for creating plugins
+
 #[cfg(test)]
 mod test {
     use quick_js::{Context, JsValue};
@@ -7,28 +9,19 @@ mod test {
         let context = Context::new().unwrap();
 
         // Eval.
-
         let value = context.eval("1 + 2").unwrap();
         assert_eq!(value, JsValue::Int(3));
-
         let value = context
             .eval_as::<String>(" var x = 100 + 250; x.toString() ")
             .unwrap();
         assert_eq!(&value, "350");
 
         // Callbacks.
-
         context
             .add_callback("myCallback", |a: i32, b: i32| a + b)
             .unwrap();
-
-        context
-            .eval(
-                r#"
-    // x will equal 30
-    var x = myCallback(10, 20);
-"#,
-            )
-            .unwrap();
+        let callback_demo = include_str!("./js/callback_demo.js");
+        let callback_val = context.eval(callback_demo).unwrap();
+        assert_eq!(callback_val, JsValue::Int(30));
     }
 }
