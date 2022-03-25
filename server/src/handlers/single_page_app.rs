607fec01891f45b3248b78affe86550ea7db457b
Joep Meindertsma <joep@ontola.io>
2022-03-25 13:27:02 +0100

    #353 rename static folder to templates

--- server/src/handlers/single_page_app.rs
+++ server/src/handlers/single_page_app.rs
@@ -6,7 +6,7 @@ use actix_web::HttpResponse;
 pub async fn single_page(
     appstate: actix_web::web::Data<AppState>,
 ) -> AtomicServerResult<HttpResponse> {
-    let template = include_str!("../../static/atomic-data-browser.html");
+    let template = include_str!("../../templates/atomic-data-browser.html");
     let body = template
         .replace("{ script }", &appstate.config.opts.script)
         .replace("{ asset_url }", &appstate.config.opts.asset_url);
