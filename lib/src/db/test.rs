70893dc72ca0963dde99953aaa35eb6ecb6d3f44
AlexMikhalev <alex@metacortex.engineer>
2022-04-19 12:09:58 +0100

    As bytes instead bincode (#385)
    * Added failed test at all resources - boolean parameter ignored
    
    Signed-off-by: AlexMikhalev <alex.mikhalev@gmail.com>
    
    * remove .DS_Store
    
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>
    
    * as butes for subject
    
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>
    
    * Attempted merge

--- lib/src/db/test.rs
+++ lib/src/db/test.rs
@@ -432,17 +432,6 @@ fn test_db_resources_all() {
 }
 
 #[test]
-fn test_db_resources_all() {
-    let store = &Db::init_temp("resources_all").unwrap();
-    let res_no_include = store.all_resources(false).len();
-    let res_include = store.all_resources(true).len();
-    assert!(
-        res_include > res_no_include,
-        "Amount of results should be higher for include_external"
-    );
-}
-
-#[test]
 /// Changing these values actually correctly updates the index.
 fn index_invalidate_cache() {
     let store = &Db::init_temp("invalidate_cache").unwrap();
