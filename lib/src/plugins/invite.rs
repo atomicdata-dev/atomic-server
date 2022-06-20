43ff9a6cb04557c9b636a22d21b25c87be60619e
Joep Meindertsma <joep@ontola.io>
2022-06-20 13:57:36 +0200

    #450 invite usese push_propval

--- lib/src/plugins/invite.rs
+++ lib/src/plugins/invite.rs
@@ -130,27 +130,8 @@ pub fn add_rights(
     // Get the Resource that the user is being invited to
     let mut target = store.get_resource(target)?;
     let right = if write { urls::WRITE } else { urls::READ };
-    let mut rights_vector: Vec<String> = match target.get(right) {
-        // Rights have been set, add to the list
-        Ok(val) => {
-            let vec = val
-                .to_subjects(None)
-                .map_err(|_| "Invalid value for rights")?;
-            // If the vector already contains the agent, throw an error;
-            for a in &vec {
-                if a == agent {
-                    return Ok(());
-                }
-            }
-            vec
-        }
-        // No rights have been set, create a new vector
-        Err(_) => Vec::new(),
-    };
 
-    rights_vector.push(agent.to_string());
-
-    target.set_propval(right.into(), rights_vector.into(), store)?;
+    target.push_propval(right, agent.into(), true)?;
     target
         .save_locally(store)
         .map_err(|e| format!("Unable to save updated target resource. {}", e))?;
