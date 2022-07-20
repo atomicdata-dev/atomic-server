d813914768345381bc8ba51fb73c24bfac6837a6
Joep Meindertsma <joep@ontola.io>
2022-07-20 17:43:39 +0200

    #469 first build in CI before test

--- server/src/tests.rs
+++ server/src/tests.rs
@@ -160,25 +160,23 @@ fn get_body(resp: ServiceResponse) -> String {
     String::from_utf8(bytes.as_ref().into()).unwrap()
 }
 
-use assert_cmd::Command;
-
 // CLI tests
 
 #[test]
 fn wrong_command() {
-    let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+    let mut cmd = assert_cmd::Command::cargo_bin("atomic-server").unwrap();
     cmd.args(&["non-existent-command"]).assert().failure();
 }
 
 #[test]
 fn help() {
-    let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+    let mut cmd = assert_cmd::Command::cargo_bin("atomic-server").unwrap();
     cmd.args(&["help"]).assert().success();
 }
 
 // #[test]
 // fn import_file() {
-//     let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+//     let mut cmd = assert_cmd::Command::cargo_bin("atomic-server").unwrap();
 //     cmd.current_dir(".").unwrap();
 //     cmd.args(&["import", "--file", "./lib/test_files/local_id.json"])
 //         .assert()
