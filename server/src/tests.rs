7782f6002fbfb1f218280959fce6b52d753c6cf3
Joep Meindertsma <joep@ontola.io>
2022-07-21 10:34:45 +0200

    #469 fix server cli tests

--- server/src/tests.rs
+++ server/src/tests.rs
@@ -159,26 +159,3 @@ fn get_body(resp: ServiceResponse) -> String {
     let bytes = boxbody.try_into_bytes().unwrap();
     String::from_utf8(bytes.as_ref().into()).unwrap()
 }
-
-// CLI tests
-
-#[test]
-fn wrong_command() {
-    let mut cmd = assert_cmd::Command::cargo_bin("atomic-server").unwrap();
-    cmd.args(&["non-existent-command"]).assert().failure();
-}
-
-#[test]
-fn help() {
-    let mut cmd = assert_cmd::Command::cargo_bin("atomic-server").unwrap();
-    cmd.args(&["help"]).assert().success();
-}
-
-// #[test]
-// fn import_file() {
-//     let mut cmd = assert_cmd::Command::cargo_bin("atomic-server").unwrap();
-//     cmd.current_dir(".").unwrap();
-//     cmd.args(&["import", "--file", "./lib/test_files/local_id.json"])
-//         .assert()
-//         .success();
-// }
