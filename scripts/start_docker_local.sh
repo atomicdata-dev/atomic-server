f75035db3a84b7e57ba9fe22dad6d752f730773d
AlexMikhalev <alex@metacortex.engineer>
2022-07-31 13:04:15 +0100

    Added local docker start script
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- scripts/start_docker_local.sh
+++ scripts/start_docker_local.sh
@@ -4,4 +4,4 @@ docker run --rm --hostname dns.mageddo -d --name dns-proxy-server -p 5380:5380 \
   -v /etc/resolv.conf:/etc/resolv.conf \
   defreitas/dns-proxy-server 
 
-docker run -e ATOMIC_SERVER_URL=atomic.dev.intranet --rm -d -p 80:8080 --name="atomic-server" --hostname atomic.dev.intranet atomic-server
+docker run -e ATOMIC_SERVER_URL=http://atomic.dev.intranet --rm -p 80:8080 --name="atomic-server" --hostname atomic.dev.intranet atomic-server
