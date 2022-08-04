3df9f081a269a66b0a9c659fca11bf8cff5498a1
Polle Pas <polle@argu.co>
2022-08-04 16:13:59 +0200

    #432 Add fetchbookmark route

--- lib/src/plugins/bookmark.rs
+++ lib/src/plugins/bookmark.rs
@@ -1,7 +1,9 @@
 use kuchiki::{traits::TendrilSink, NodeRef};
 use lol_html::{element, rewrite_str, text, ElementContentHandlers, RewriteStrSettings, Selector};
-use std::{borrow::Cow, string::FromUtf8Error};
+use std::{borrow::Cow, collections::HashMap, string::FromUtf8Error};
 use url::Url;
+use urlencoding::encode;
+use uuid::Uuid;
 
 use crate::{
     client::fetch_body, endpoints::Endpoint, errors::AtomicResult, urls, values::Value,
@@ -12,7 +14,7 @@ type Handler<'s, 'h> = Vec<(Cow<'s, Selector>, ElementContentHandlers<'h>)>;
 
 pub fn bookmark_endpoint() -> Endpoint {
     Endpoint {
-        path: "/bookmark".to_string(),
+        path: "/fetchbookmark".to_string(),
         params: [urls::URL.to_string(), urls::NAME.to_string()].into(),
         description: "The website will be fetched and parsed by the server. The data will then be added as a markdown document that is fully indexed.".to_string(),
         shortname: "bookmark".to_string(),
@@ -44,15 +46,24 @@ fn handle_bookmark_request(
     }
 
     let path = path.unwrap();
+    let mut name = name.unwrap();
 
     let mut resource = Resource::new(url.to_string());
     resource.set_class(urls::BOOKMARK.into())?;
     resource.set_propval_string(urls::URL.into(), &path.clone(), store)?;
-    resource.set_propval_string(urls::NAME.into(), &name.unwrap(), store)?;
 
+    // Fetch the data and create a parser from it.
     let content = fetch_data(&path)?;
     let mut parser = Parser::from_html(&path, &content);
 
+    // Extract the title from the HTML
+    if let Some(title) = parser.get_title() {
+        name = title;
+    }
+
+    resource.set_propval_string(urls::NAME.into(), &name, store)?;
+
+    // Clean and transform the HTML to markdown.
     if let Ok(cleaned_html) = parser.clean_document() {
         let md = html2md::parse_html(&cleaned_html);
 
@@ -84,6 +95,7 @@ struct Parser {
     internal_html: String,
     root_element: String,
     anchor_text_buffer: std::rc::Rc<std::cell::RefCell<String>>,
+    svg_map: HashMap<String, String>,
 }
 
 impl Parser {
@@ -93,6 +105,7 @@ impl Parser {
             internal_html: html.to_string(),
             root_element: "body".to_string(),
             anchor_text_buffer: std::rc::Rc::new(std::cell::RefCell::new(String::new())),
+            svg_map: HashMap::new(),
         }
     }
 
@@ -107,8 +120,19 @@ impl Parser {
         return result;
     }
 
+    pub fn get_title(&self) -> Option<String> {
+        let document = kuchiki::parse_html().one(self.internal_html.clone());
+
+        if let Ok(title_element) = document.select_first("title") {
+            return Some(title_element.text_contents());
+        } else {
+            return None;
+        }
+    }
+
     pub fn clean_document(&mut self) -> Result<String, AtomicError> {
         self.select_best_node()?;
+        self.index_svgs()?;
         self.process_html()?;
 
         return Ok(self.internal_html.clone());
@@ -157,16 +181,48 @@ impl Parser {
         }
     }
 
+    fn index_svgs(&mut self) -> Result<(), AtomicError> {
+        let document = kuchiki::parse_html().one(self.internal_html.clone());
+
+        for node in document.select("svg").unwrap() {
+            let id = Uuid::new_v4().to_string();
+
+            let svg = Parser::serialize(node.clone().as_node().clone()).unwrap();
+
+            // Tag the element with an ID so the rewriter can find it later.
+            node.attributes
+                .borrow_mut()
+                .insert("id".to_string(), id.clone());
+
+            self.svg_map.insert(id, encode(&svg).into_owned());
+        }
+
+        match Parser::serialize(document) {
+            Ok(result) => {
+                self.internal_html = result;
+                Ok(())
+            }
+            Err(e) => Err(AtomicError {
+                message: format!("Error serializing node: {}", e),
+                error_type: AtomicErrorType::OtherError,
+            }),
+        }
+    }
+
     fn process_html(&mut self) -> Result<(), AtomicError> {
         match rewrite_str(
             &self.internal_html,
             RewriteStrSettings {
                 element_content_handlers: vec![
+                    self.unpack_noscript_handler(),
                     self.remove_unwanted_elements_handler(),
-                    self.strip_image_dimensions_handler(),
+                    self.transform_figures_handler(),
+                    self.transform_figcaptions_handler(),
                     self.resolve_relative_path_handler(),
-                    self.simplify_link_text(),
-                    self.trim_link_text(),
+                    self.simplify_link_text_handler(),
+                    self.trim_link_text_handler(),
+                    self.convert_svg_to_image_handler(),
+                    self.strip_image_dimensions_handler(),
                 ]
                 .into_iter()
                 .flatten()
@@ -185,11 +241,19 @@ impl Parser {
         }
     }
 
+    fn unpack_noscript_handler<'h, 's>(&self) -> Handler<'s, 'h> {
+        return vec![element!("noscript", |el| {
+            el.remove_and_keep_content();
+            Ok(())
+        })];
+    }
+
     fn remove_unwanted_elements_handler<'h, 's>(&self) -> Handler<'s, 'h> {
+        let elements = "nav, footer, iframe, script, aside, style, button, label".to_string();
         let selector = if self.root_element == "article" {
-            "nav, footer, iframe, script, aside, style"
+            elements
         } else {
-            "header, nav, footer, iframe, script, aside, style"
+            elements + ", header"
         };
 
         return vec![element!(selector, |el| {
@@ -228,7 +292,24 @@ impl Parser {
         })];
     }
 
-    fn simplify_link_text<'h>(&self) -> Handler<'h, 'h> {
+    fn convert_svg_to_image_handler<'h>(&'h self) -> Handler<'h, 'h> {
+        return vec![element!("svg", |el| {
+            let id = el.get_attribute("id").unwrap();
+            let svg = self.svg_map.get(&id).unwrap();
+
+            el.set_tag_name("img")?;
+            el.remove_attribute("height");
+            el.remove_attribute("width");
+            el.remove_attribute("viewBox");
+            el.remove_attribute("fill");
+            el.remove_attribute("xmlns");
+            el.set_attribute("src", &format!("data:image/svg+xml;utf8,{}", &svg))?;
+            el.set_inner_content("", lol_html::html_content::ContentType::Html);
+            Ok(())
+        })];
+    }
+
+    fn simplify_link_text_handler<'h>(&self) -> Handler<'h, 'h> {
         return vec![element!("a *", |el| {
             let tag_name = el.tag_name().to_lowercase();
             if tag_name != "img" && tag_name != "picture" {
@@ -239,15 +320,35 @@ impl Parser {
         })];
     }
 
-    fn trim_link_text<'h>(&'h self) -> Handler<'h, 'h> {
+    fn transform_figures_handler<'h>(&self) -> Handler<'h, 'h> {
+        return vec![element!("figure", |el| {
+            el.remove_and_keep_content();
+            Ok(())
+        })];
+    }
+
+    fn transform_figcaptions_handler<'h>(&self) -> Handler<'h, 'h> {
+        return vec![element!("figcaption", |el| {
+            el.set_tag_name("P").unwrap();
+            Ok(())
+        })];
+    }
+
+    fn trim_link_text_handler<'h>(&'h self) -> Handler<'h, 'h> {
         return vec![
             element!("a", |el| {
                 self.anchor_text_buffer.borrow_mut().clear();
                 let buffer = self.anchor_text_buffer.clone();
+                let href = el.get_attribute("href").unwrap_or("link".to_string());
 
                 el.on_end_tag(move |end| {
                     let s = buffer.borrow();
-                    let text = s.as_str().trim();
+                    let mut text = s.as_str().trim();
+
+                    if text.len() == 0 {
+                        text = &href;
+                    }
+
                     end.before(text, lol_html::html_content::ContentType::Text);
 
                     Ok(())
@@ -272,6 +373,31 @@ impl Parser {
 #[cfg(test)]
 mod tests {
     #[test]
+    fn test_select_best_element() {
+        let html = r#"<html><body><header><nav>navigation</nav></header><iframe></iframe><article><header>article header</header><p>lor em ip sum<p/></article></body></html>"#;
+        let mut parser = super::Parser::from_html("https://bla.com", html);
+
+        let parsed_html = parser.clean_document().unwrap();
+        let md = html2md::parse_html(&parsed_html);
+
+        assert_eq!(md, "article header\n\nlor em ip sum");
+    }
+
+    #[test]
+    fn test_resolve_relative_paths() {
+        let html = r#"<article><p>The <a href="/animals/chicken.html">chicken</a> is an animal</p></article>"#;
+        let mut parser = super::Parser::from_html("https://bla.com", html);
+
+        let parsed_html = parser.clean_document().unwrap();
+        let md = html2md::parse_html(&parsed_html);
+
+        assert_eq!(
+            md,
+            "The [chicken](https://bla.com/animals/chicken.html) is an animal"
+        );
+    }
+
+    #[test]
     fn test_clean_link() {
         let html = r#"<html><body><a href="https://bla.com"><div>Het is</div><div>taco tijd</div></a></body></html>"#;
         let mut parser = super::Parser::from_html("https://bla.com", html);
@@ -279,7 +405,35 @@ mod tests {
         let parsed_html = parser.clean_document().unwrap();
         let md = html2md::parse_html(&parsed_html);
 
-        println!("{}", md);
         assert_eq!(md, "[Het is taco tijd](https://bla.com)");
+    }
+
+    #[test]
+    fn parse_images() {
+        let html = r#"<html><body><img alt="Netflix" height="369" src="https://tweakers.net/i/Imo-YDw3aJMOUg7-aMw2OC0lk6Q=/656x/filters:strip\_icc():strip\_exif()/i/2004517792.jpeg?f=imagenormal" width="656"></body></html>"#;
+        let mut parser = super::Parser::from_html("https://bla.com", html);
+
+        let parsed_html = parser.clean_document().unwrap();
+        let md = html2md::parse_html(&parsed_html);
+
+        assert_eq!(
+            md,
+            r#"![Netflix](https://tweakers.net/i/Imo-YDw3aJMOUg7-aMw2OC0lk6Q=/656x/filters:strip\_icc():strip\_exif()/i/2004517792.jpeg?f=imagenormal)"#
+        );
+    }
+
+    #[test]
+    fn convert_svg() {
+        let html =
+            r#"<html><body><svg><path d="M 10 10 H 90 V 90 H 10 L 10 10"/></svg></body></html>"#;
+        let mut parser = super::Parser::from_html("https://bla.com", html);
+
+        let parsed_html = parser.clean_document().unwrap();
+        let md = html2md::parse_html(&parsed_html);
+
+        assert_eq!(
+            md,
+            r#"![](data:image/svg+xml;utf8,%3Csvg%3E%3Cpath%20d%3D%22M%2010%2010%20H%2090%20V%2090%20H%2010%20L%2010%2010%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E)"#
+        );
     }
 }
