00a9a7ad362d4cea1ec4c62aee9d24cc7a375423
Thom van Kalkeren <thom@argu.co>
2022-10-26 16:18:16 +0200

    chore: update browser bundle

--- server/e2e_tests/e2e-generated.spec.ts
+++ server/e2e_tests/e2e-generated.spec.ts
@@ -330,9 +330,9 @@ test.describe('data-browser', async () => {
     ]);
     await fileChooser.setFiles(demoFile);
     await page.click(`[data-test]:has-text("${demoFileName}")`);
-    await expect(
-      await page.locator('[data-test="image-viewer"]'),
-    ).toBeVisible();
+    const image = await page.locator('[data-test="image-viewer"]');
+    await expect(image).toBeVisible();
+    await expect(image).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
   });
 
   test('chatroom', async ({ page, browser }) => {
