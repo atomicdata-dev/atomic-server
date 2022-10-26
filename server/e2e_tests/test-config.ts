580e43a105f83e2de0384a06f6dd68d9b3189578
Thom van Kalkeren <thom@argu.co>
2022-10-26 16:18:16 +0200

    chore: update browser bundle

--- server/e2e_tests/test-config.ts
+++ server/e2e_tests/test-config.ts
@@ -1,9 +1,9 @@
 import { TestConfig } from "./e2e-generated.spec";
-const demoFileName = 'logo.svg';
+const demoFileName = 'testimage.svg';
 
 export const testConfig: TestConfig = {
   demoFileName,
-  demoFile: `../../${demoFileName}`,
+  demoFile: `./${demoFileName}`,
   demoInviteName: 'document demo',
   serverUrl: 'http://localhost:9883',
   frontEndUrl: 'http://localhost:9883',
