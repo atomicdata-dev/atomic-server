7535eeceb75c88c2ff585d1b35eb8b45d061ba8f
Joep Meindertsma <joep@ontola.io>
2023-02-04 17:32:49 +0100

    WIP #578 #577 # 511

--- server/src/handlers/single_page_app.rs
+++ server/src/handlers/single_page_app.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+use std::fmt::Formatter;
+
 use crate::{appstate::AppState, errors::AtomicServerResult};
 use actix_web::HttpResponse;
 
@@ -6,10 +9,21 @@ use actix_web::HttpResponse;
 pub async fn single_page(
     appstate: actix_web::web::Data<AppState>,
 ) -> AtomicServerResult<HttpResponse> {
-    let template = include_str!("../../templates/atomic-data-browser.html");
-    let body = template
-        .replace("{ script }", &appstate.config.opts.script)
-        .replace("{ asset_url }", &appstate.config.opts.asset_url);
+    let template = include_str!("../../app_assets/index.html");
+    // TODO Get the agent
+    // let subject = urls::AGENT;
+    let subject = "http://localhost:9883/Folder/ltg0aypur9d";
+    let meta_tags: MetaTags = if let Ok(resource) =
+        appstate
+            .store
+            .get_resource_extended(subject, true, Some(urls::PUBLIC_AGENT))
+    {
+        resource.into()
+    } else {
+        MetaTags::default()
+    };
+
+    let body = template.replace("<!-- { inject_html_head } -->", &meta_tags.to_string());
 
     let resp = HttpResponse::Ok()
         .content_type("text/html")
@@ -22,4 +36,114 @@ pub async fn single_page(
         .body(body);
 
     Ok(resp)
+}
+
+use atomic_lib::urls;
+use atomic_lib::Resource;
+use atomic_lib::Storelike;
+
+/* HTML tags for social media and link previews. Also includes JSON-AD body of the requested resource, if publicly available. */
+struct MetaTags {
+    description: String,
+    title: String,
+    image: String,
+    json: Option<String>,
+}
+
+impl From<Resource> for MetaTags {
+    fn from(r: Resource) -> Self {
+        let description = if let Ok(d) = r.get(urls::DESCRIPTION) {
+            d.to_string()
+        } else {
+            "Open this resource in your browser to view its contents.".to_string()
+        };
+        let title = if let Ok(d) = r.get(urls::NAME) {
+            d.to_string()
+        } else {
+            "Atomic Server".to_string()
+        };
+        let image = if let Ok(d) = r.get(urls::DOWNLOAD_URL) {
+            // TODO: check if thefile is actually an image
+            d.to_string()
+        } else {
+            "/default_social_preview.jpg".to_string()
+        };
+        let json = if let Ok(serialized) = r.to_json_ad() {
+            Some(serialized)
+        } else {
+            None
+        };
+        Self {
+            description,
+            title,
+            image,
+            json,
+        }
+    }
+}
+
+impl Default for MetaTags {
+    fn default() -> Self {
+        Self {
+            description: "Sign in to view this resource".to_string(),
+            title: "Atomic Server".to_string(),
+            image: "/default_social_preview.jpg".to_string(),
+            json: None,
+        }
+    }
+}
+
+impl Display for MetaTags {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let description = escape_html(&self.description);
+        let image = &self.image;
+        let title = escape_html(&self.title);
+
+        write!(
+            f,
+            "<meta name=\"description\" content=\"{description}\">
+<meta property=\"og:title\" content=\"{title}\">
+<meta property=\"og:description\" content=\"{description}\">
+<meta property=\"og:image\" content=\"{image}\">
+<meta property=\"twitter:card\" content=\"summary_large_image\">
+<meta property=\"twitter:title\" content=\"{title}\">
+<meta property=\"twitter:description\" content=\"{description}\">
+<meta property=\"twitter:image\" content=\"{image}\">"
+        )?;
+        if let Some(json_unsafe) = &self.json {
+            let json_base64 = base64::encode(json_unsafe);
+            write!(
+                f,
+                "\n<meta property=\"json-ad-initial\" content=\"{}\">",
+                json_base64
+            )?;
+        };
+        Ok(())
+    }
+}
+
+fn escape_html(s: &str) -> String {
+    s.replace('<', "&lt;")
+        .replace('>', "&gt;")
+        .replace('&', "&amp;")
+        .replace('\'', "&#x27;")
+        .replace('"', "&quot;")
+        .replace('/', "&#x2F;")
+}
+
+#[cfg(test)]
+mod test {
+    use super::MetaTags;
+
+    #[test]
+    // Malicious test: try escaping html and adding script tag
+    fn evil_meta_tags() {
+        let html = MetaTags {
+            description: "\"<script>alert('evil')</script>\"".to_string(),
+            ..Default::default()
+        }
+        .to_string();
+        println!("{}", html);
+        assert!(!html.contains("<script>"));
+    }
 }
