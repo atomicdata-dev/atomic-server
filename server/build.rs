15f12f7622d3d928d69f8e213fd2670f199ce169
Joep Meindertsma <joep@ontola.io>
2023-07-27 14:49:37 +0200

    #216 fix auto build JS assets

--- server/build.rs
+++ server/build.rs
@@ -1,15 +1,72 @@
+use std::time::SystemTime;
+
 use static_files::resource_dir;
 
-fn main() -> std::io::Result<()> {
-    let js_build_path = "../browser/data-browser/dist";
+const JS_DIST: &str = "../browser/data-browser/dist";
+const SRC_BROWSER: &str = "../browser/data-browser/src";
 
-    if std::fs::read_dir(js_build_path).is_err() {
-        println!("Running `pnpm run build` in the data-browser folder...");
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
+/// Check if any JS files were modified since the last build
+fn should_build() -> bool {
+    if let Ok(dist) = std::fs::metadata(format!("{}/index.html", JS_DIST)) {
+        let dist_time = dist
+            .modified()
+            .unwrap()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        for entry in walkdir::WalkDir::new(SRC_BROWSER)
+            .into_iter()
+            .filter_map(|e| {
+                // ignore ds store
+                if let Ok(e) = e {
+                    if e.path().to_str().unwrap().contains(".DS_Store") {
+                        return None;
+                    }
+                    Some(e)
+                } else {
+                    None
+                }
+            })
+        {
+            if entry.path().is_file() {
+                let src_time = entry
+                    .metadata()
+                    .unwrap()
+                    .modified()
+                    .unwrap()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap();
+                if src_time >= dist_time {
+                    p!("Source file modified: {:?}, rebuilding...", entry.path());
+                    return true;
+                }
+            }
+        }
+
+        p!("No changes in JS, skipping build.");
+        false
+    } else {
+        p!("No dist folder found, building...");
+        true
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    println!("cargo:rerun-if-changed=../browser");
+
+    if should_build() {
+        p!("pnpm install...");
         std::process::Command::new("pnpm")
             .current_dir("../browser")
             .args(["install"])
             .output()
             .expect("failed to install deps");
+        p!("pnpm build...");
         std::process::Command::new("pnpm")
             .current_dir("../browser")
             .args(["run", "build"])
@@ -17,9 +74,9 @@ fn main() -> std::io::Result<()> {
             .expect("failed to build js bundle");
     }
 
-    resource_dir(js_build_path)
+    resource_dir(JS_DIST)
         .build()
-        .unwrap_or_else(|_| panic!("failed to open data browser assets from {}", js_build_path));
+        .unwrap_or_else(|_| panic!("failed to open data browser assets from {}", JS_DIST));
 
     Ok(())
 }
