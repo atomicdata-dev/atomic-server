90d89c7d0c165c60672180f0e78892612c379bf3
Joep Meindertsma <joep@ontola.io>
2023-08-30 15:56:48 +0200

    Try fix e2e test title

--- browser/data-browser/tests/test-utils.ts
+++ browser/data-browser/tests/test-utils.ts
@@ -54,7 +54,9 @@ export const before = async ({ page }) => {
 
 export async function setTitle(page: Page, title: string) {
   await page.locator(editableTitle).click();
-  await page.fill(editableTitle, title);
+  await page.locator(`${editableTitle} > input`);
+  await page.type(editableTitle, title);
+  await page.keyboard.press('Escape');
   // await page.waitForTimeout(500);
 }
 
@@ -194,13 +196,13 @@ export async function changeDrive(subject: string, page: Page) {
 }
 
 export async function editTitle(title: string, page: Page) {
+  await page.locator(`${editableTitle} > h1`);
   await page.locator(editableTitle).click();
-  await page.waitForTimeout(100);
   await page.locator(`${editableTitle} > input`);
   await page.fill(editableTitle, title);
   await page.keyboard.press('Escape');
   // Make sure the commit is processed
-  await page.waitForTimeout(300);
+  // await page.waitForTimeout(300);
 }
 
 export async function clickSidebarItem(text: string, page: Page) {
