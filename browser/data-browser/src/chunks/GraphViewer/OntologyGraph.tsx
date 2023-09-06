d140ebc230e812838bbc8f4f949ae17025ad8043
Polle Pas <polleps@gmail.com>
2023-09-06 13:09:46 +0200

    #648 Display graph in ontology view

---
+++ browser/data-browser/src/chunks/GraphViewer/OntologyGraph.tsx
@@ -0,0 +1,75 @@
+import { Resource, useStore } from '@tomic/react';
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  Controls,
+  useReactFlow,
+  Node,
+  ReactFlowProvider,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+import './reactFlowOverrides.css';
+import { buildGraph } from './buildGraph';
+import { FloatingEdge } from './FloatingEdge';
+import { useGraph } from './useGraph';
+import { useEffectOnce } from '../../hooks/useEffectOnce';
+import { toAnchorId } from '../../views/OntologyPage/toAnchorId';
+
+const edgeTypes = {
+  floating: FloatingEdge,
+};
+
+interface OntologyGraphProps {
+  ontology: Resource;
+}
+
+/**
+ * !ASYNC COMPONENT, DO NOT IMPORT DIRECTLY!
+ * Displays an ontology as a graph.
+ */
+export default function OntologyGraph({
+  ...props
+}: OntologyGraphProps): JSX.Element {
+  return (
+    <ReactFlowProvider>
+      <OntologyGraphInner {...props} />
+    </ReactFlowProvider>
+  );
+}
+
+function OntologyGraphInner({ ontology }: OntologyGraphProps): JSX.Element {
+  const store = useStore();
+  const { fitView } = useReactFlow();
+
+  const { nodes, edges, setGraph, handleNodeChange, handleNodeDoubleClick } =
+    useGraph(ontology);
+
+  useEffectOnce(() => {
+    buildGraph(ontology, store).then(([n, e]) => {
+      setGraph(n, e);
+
+      requestAnimationFrame(() => {
+        fitView();
+      });
+    });
+  });
+
+  const handleClick = useCallback((_: React.MouseEvent, node: Node) => {
+    const domId = toAnchorId(node.id);
+
+    document.getElementById(domId)?.scrollIntoView({ behavior: 'smooth' });
+  }, []);
+
+  return (
+    <ReactFlow
+      fitView
+      nodes={nodes}
+      edges={edges}
+      edgeTypes={edgeTypes}
+      onNodesChange={handleNodeChange}
+      onNodeClick={handleClick}
+      onNodeDoubleClick={handleNodeDoubleClick}
+    >
+      <Controls position='top-left' showInteractive={false} />
+    </ReactFlow>
+  );
+}
---
+++ browser/data-browser/src/chunks/GraphViewer/useGraph.ts
@@ -0,0 +1,156 @@
+import {
+  Edge,
+  Node,
+  NodeChange,
+  NodePositionChange,
+  applyNodeChanges,
+} from 'reactflow';
+import { EdgeData, NodeData, applyNodeStyling } from './buildGraph';
+import { useCallback, useMemo, useState } from 'react';
+import Dagre from '@dagrejs/dagre';
+import { useTheme } from 'styled-components';
+import { Resource, urls, useString } from '@tomic/react';
+
+interface CustomNodePositioning {
+  [key: string]: [x: number, y: number];
+}
+
+type UseNodeReturn = {
+  nodes: Node<NodeData>[];
+  edges: Edge<EdgeData>[];
+  setGraph: (nodes: Node<NodeData>[], edges: Edge<EdgeData>[]) => void;
+  handleNodeChange: (changes: NodeChange[]) => void;
+  handleNodeDoubleClick: (event: React.MouseEvent, node: Node) => void;
+};
+
+const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (nodes: Node[], edges: Edge[]) => {
+  g.setGraph({ rankdir: 'vertical', ranksep: 70 });
+
+  edges.forEach(edge => g.setEdge(edge.source, edge.target));
+  nodes.forEach(node =>
+    g.setNode(node.id, { label: node, width: 120, height: 100 }),
+  );
+
+  Dagre.layout(g);
+
+  return {
+    positionedNodes: nodes.map(node => {
+      const { x, y } = g.node(node.id);
+
+      return { ...node, position: { x, y } };
+    }),
+    positionedEdges: edges,
+  };
+};
+
+const placeNodesInSpace = (
+  nodes: Node[],
+  edges: Edge[],
+  customPositioning: CustomNodePositioning,
+): [nodes: Node[], edges: Edge[]] => {
+  const { positionedNodes, positionedEdges } = getLayoutedElements(
+    nodes,
+    edges,
+  );
+
+  const ajustedNodes = positionedNodes.map(node => {
+    if (customPositioning[node.id]) {
+      const [x, y] = customPositioning[node.id];
+
+      return { ...node, position: { x, y }, positionAbsolute: { x, y } };
+    }
+
+    return node;
+  });
+
+  return [ajustedNodes, positionedEdges];
+};
+
+export function useGraph(ontology: Resource): UseNodeReturn {
+  const theme = useTheme();
+
+  const [customPositioningSTR, setCustomPositioningSTR] = useString(
+    ontology,
+    urls.properties.ontology.customNodePositioning,
+    { commit: true },
+  );
+
+  const customPositioning = useMemo(
+    () => JSON.parse(customPositioningSTR || '{}'),
+    [customPositioningSTR],
+  );
+
+  const [nodes, setNodes] = useState<Node<NodeData>[]>([]);
+  const [edges, setEdges] = useState<Edge<EdgeData>[]>([]);
+  const [lastPositionChange, setLastPositionChange] =
+    useState<NodePositionChange>();
+
+  const setGraph = useCallback(
+    (_nodes: Node<NodeData>[], _edges: Edge<EdgeData>[]) => {
+      const [positionedNodes, positionedEdges] = placeNodesInSpace(
+        _nodes,
+        _edges,
+        customPositioning,
+      );
+      setNodes(applyNodeStyling(positionedNodes, theme));
+      setEdges(positionedEdges);
+    },
+    [theme, customPositioning],
+  );
+
+  const handleNodeDoubleClick = useCallback(
+    async (_e: React.MouseEvent, node: Node) => {
+      const newCustomPositioning = {
+        ...customPositioning,
+      };
+
+      delete newCustomPositioning[node.id];
+
+      await setCustomPositioningSTR(JSON.stringify(newCustomPositioning));
+
+      const [positionedNodes] = placeNodesInSpace(
+        nodes,
+        edges,
+        newCustomPositioning,
+      );
+
+      setNodes(positionedNodes);
+    },
+    [customPositioning, nodes, edges],
+  );
+
+  const handleNodeChange = useCallback(
+    (changes: NodeChange[]) => {
+      const change = changes[0];
+
+      if (change.type === 'position') {
+        if (change.dragging) {
+          setLastPositionChange(change);
+        } else {
+          setCustomPositioningSTR(
+            JSON.stringify({
+              ...customPositioning,
+              [change.id]: [
+                lastPositionChange!.positionAbsolute?.x,
+                lastPositionChange!.positionAbsolute?.y,
+              ],
+            }),
+          );
+        }
+      }
+
+      setNodes(prev => applyNodeChanges(changes, prev));
+    },
+    [customPositioning, lastPositionChange],
+  );
+
+  return {
+    nodes,
+    edges,
+    setGraph,
+    handleNodeChange,
+    handleNodeDoubleClick,
+  };
+}
