92b446eb4a2c647b1a471777500e697386673acc
Joonas Lipping <lippinj@outlook.com>
2024-07-13 10:59:52 +0300

    Add test programs

--- browser/vihreat-ohjelmat/src/ViewIndex.tsx
+++ browser/vihreat-ohjelmat/src/ViewIndex.tsx
@@ -3,6 +3,16 @@ export function ViewIndex(): JSX.Element {
     <div className='vo-container'>
       <h1>Ohjelmat</h1>
       <p><a href="/ohjelmat/p0">Tietopoliittinen ohjelma</a></p>
+      <h2>Testiohjelmat</h2>
+      <p>
+        <a href="/ohjelmat/px_luo">Ohjelmaluonnos (ei hyväksytty)</a>
+        <br />
+        <a href="/ohjelmat/px_hyv">Hyväksytty ohjelma</a>
+        <br />
+        <a href="/ohjelmat/px_van">Vanhentunut ohjelma</a>
+        <br />
+        <a href="/ohjelmat/px_poi">Poistunut ohjelma</a>
+      </p>
     </div>
   );
 }
--- vihreat-data/generate.py
+++ vihreat-data/generate.py
@@ -3,6 +3,8 @@ import generate_ld
 
 generate_ld.io.write(generate_ld.ontology.build(), "ontology")
 
+# Oikeat ohjelmat...
+
 generate_ld.io.write(
     generate_ld.program.build(
         "md/tietopoliittinen-ohjelma.md",
@@ -12,4 +14,32 @@ generate_ld.io.write(
         approved_on="2021-05-16",
     ),
     "tietopoliittinen-ohjelma",
+)
+
+
+# Testiohjelmat...
+
+
+def generate_test(name, kind, **kwargs):
+    generate_ld.io.write(
+        generate_ld.program.build(
+            "md/tietopoliittinen-ohjelma.md",
+            name=name,
+            title="Lorem ipsum dolor sit amet",
+            subtitle=f"TESTIOHJELMA ({kind})",
+            **kwargs,
+        ),
+        name,
+    )
+
+
+generate_test("px_luo", "luonnos")
+generate_test("px_hyv", "voimassa", approved_on="2021-01-01")
+generate_test("px_van", "vanhentunut", approved_on="2021-01-01", stale_on="2022-05-03")
+generate_test(
+    "px_poi",
+    "poistunut",
+    approved_on="2021-01-01",
+    stale_on="2022-05-03",
+    retired_on="2023-10-05",
 )
