kod-kristoff/atomic-data-rust

Register endpoint, mail, tokens, #489 #254 #544

lib/src/plugins/register.rs
@@ -1,70 +1,150 @@
 //! Creates a new Drive and optionally also an Agent.
 
+use serde::{Deserialize, Serialize};
+
 use crate::{
     agents::Agent,
-    endpoints::Endpoint,
+    email::{EmailAddress, MailAction, MailMessage},
+    endpoints::{Endpoint, HandleGetContext},
     errors::AtomicResult,
-    urls::{self, PUBLIC_AGENT},
+    urls::{self},
     values::SubResource,
-    Resource, Storelike,
+    Db, Resource, Storelike, Value,
 };
 
 pub fn register_endpoint() -> Endpoint {
     Endpoint {
       path: "/register".to_string(),
       params: [
-        urls::INVITE_PUBKEY.to_string(),
         urls::NAME.to_string(),
+        urls::EMAIL.to_string(),
       ].into(),
       description: "Allows new users to easily, in one request, make both an Agent and a Drive. This drive will be created at the subdomain of `name`.".to_string(),
       shortname: "register".to_string(),
       handle: Some(construct_register_redirect),
+      handle_post: None,
   }
 }
 
-#[tracing::instrument(skip(store))]
-pub fn construct_register_redirect(
-    url: url::Url,
-    store: &impl Storelike,
-    for_agent: Option<&str>,
-) -> AtomicResult<Resource> {
-    let requested_subject = url.to_string();
-    let mut pub_key = None;
+pub fn confirm_email_endpoint() -> Endpoint {
+    Endpoint {
+        path: "/confirmEmail".to_string(),
+        params: [urls::TOKEN.to_string(), urls::INVITE_PUBKEY.to_string()].into(),
+        description: "Confirm email address and set a key for your Agent.".to_string(),
+        shortname: "confirm-email".to_string(),
+        handle: Some(construct_confirm_email_redirect),
+        handle_post: None,
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MailConfirmation {
+    pub email: EmailAddress,
+    pub name: String,
+}
+
+#[tracing::instrument()]
+pub fn construct_register_redirect(context: HandleGetContext) -> AtomicResult<Resource> {
     let mut name_option = None;
-    for (k, v) in url.query_pairs() {
+    let mut email_option: Option<EmailAddress> = None;
+    let store = context.store;
+    for (k, v) in context.subject.query_pairs() {
         match k.as_ref() {
-            "public-key" | urls::INVITE_PUBKEY => pub_key = Some(v.to_string()),
             "name" | urls::NAME => name_option = Some(v.to_string()),
+            "email" => email_option = Some(EmailAddress::new(v.to_string())?),
             _ => {}
         }
     }
-    if pub_key.is_none() && name_option.is_none() {
+    // by default just return the Endpoint
+    if name_option.is_none() && email_option.is_none() {
         return register_endpoint().to_resource(store);
-    }
-
-    let name = if let Some(n) = name_option {
-        n
-    } else {
-        return Err("No name provided".into());
     };
 
-    let drive_creator_agent: String = if let Some(key) = pub_key {
-        let mut new = Agent::new_from_public_key(store, &key)?;
-        new.name = Some(name.clone());
+    let name = name_option.ok_or("No name provided")?;
+    let email = email_option.ok_or("No email provided")?.check_used(store)?;
+
+    // send the user an e-mail to confirm sign up
+    let store_clone = store.clone();
+    let confirmation_token_struct = MailConfirmation {
+        email: email.clone(),
+        name: name.clone(),
+    };
+    let token = crate::token::sign_claim(store, confirmation_token_struct)?;
+    let mut confirm_url = store
+        .get_server_url()
+        .clone()
+        .set_path("confirmEmail")
+        .url();
+    confirm_url.set_query(Some(&format!("token={}", token)));
+    let message = MailMessage {
+        to: email,
+        subject: "Confirm your e-mail address".to_string(),
+        body: format!("Welcome to Atomic Data, {}. Please confirm your e-mail address by clicking the link below", name),
+        action: Some(MailAction {
+            name: "Confirm e-mail address".to_string(),
+            url: confirm_url.into()
+        })
+    };
+    // async, because mails are slow
+    tokio::spawn(async move {
+        store_clone
+            .send_email(message)
+            .await
+            .unwrap_or_else(|e| tracing::error!("Error sending email: {}", e));
+    });
+
+    // Here we probably want to return some sort of SuccesMessage page.
+    // Not sure what that should be.
+    let mut resource = Resource::new_generate_subject(store);
+    resource.set_propval_string(urls::DESCRIPTION.into(), "success", store)?;
+
+    // resource.set_propval(urls::, value, store)
+
+    Ok(resource)
+}
+
+#[tracing::instrument()]
+pub fn construct_confirm_email_redirect(context: HandleGetContext) -> AtomicResult<Resource> {
+    let url = context.subject;
+    let store = context.store;
+    let mut token_opt: Option<String> = None;
+    let mut pubkey_option = None;
+
+    for (k, v) in url.query_pairs() {
+        match k.as_ref() {
+            "token" | urls::TOKEN => token_opt = Some(v.to_string()),
+            "public-key" | urls::INVITE_PUBKEY => pubkey_option = Some(v.to_string()),
+            _ => {}
+        }
+    }
+    let token = if let Some(t) = token_opt {
+        t
+    } else {
+        return confirm_email_endpoint().to_resource(store);
+    };
+    let pubkey = pubkey_option.ok_or("No public-key provided")?;
+
+    // Parse and verify the JWT token
+    let confirmation = crate::token::verify_claim::<MailConfirmation>(store, &token)?.custom;
+
+    // Create the Agent if it doesn't exist yet.
+    // Note: this happens before the drive is saved, which checks if the name is available.
+    // We get new agents that just do nothing, but perhaps that's not a problem.
+    let drive_creator_agent: String = {
+        let mut new = Agent::new_from_public_key(store, &pubkey)?;
+        new.name = Some(confirmation.name.clone());
         let net_agent_subject = new.subject.to_string();
         new.to_resource()?.save(store)?;
         net_agent_subject
-    } else if let Some(agent) = for_agent {
-        if agent == PUBLIC_AGENT {
-            return Err("No `public-key` provided.".into());
-        }
-        agent.to_string()
-    } else {
-        return Err("No `public-key` provided".into());
     };
 
     // Create the new Drive
-    let drive = crate::populate::create_drive(store, Some(&name), &drive_creator_agent, false)?;
+    let drive = crate::populate::create_drive(
+        store,
+        Some(&confirmation.name),
+        &drive_creator_agent,
+        false,
+    )?;
 
     // Add the drive to the Agent's list of drives
     let mut agent = store.get_resource(&drive_creator_agent)?;
@@ -72,6 +152,12 @@ pub fn construct_register_redirect(
         urls::DRIVES,
         SubResource::Subject(drive.get_subject().into()),
         true,
+    )?;
+    // TODO: Make sure this only works if the server sets the email address.
+    agent.set_propval(
+        urls::EMAIL.into(),
+        Value::String(confirmation.email.to_string()),
+        store,
     )?;
     agent.save_locally(store)?;
 
@@ -83,7 +169,5 @@ pub fn construct_register_redirect(
         crate::Value::AtomicUrl(drive_creator_agent),
         store,
     )?;
-    // The front-end requires the @id to be the same as requested
-    redirect.set_subject(requested_subject);
     Ok(redirect)
 }

atomicdata-dev/atomic-server

lib/src/plugins/register.rs
@@ -1,20 +1,23 @@
 //! Creates a new Drive and optionally also an Agent.
 
+use serde::{Deserialize, Serialize};
+
 use crate::{
     agents::Agent,
-    endpoints::Endpoint,
+    email::{EmailAddress, MailAction, MailMessage},
+    endpoints::{Endpoint, HandleGetContext},
     errors::AtomicResult,
-    urls::{self, PUBLIC_AGENT},
+    urls::{self},
     values::SubResource,
-    Resource, Storelike,
+    Resource, Storelike, Value,
 };
 
 pub fn register_endpoint() -> Endpoint {
     Endpoint {
       path: "/register".to_string(),
       params: [
-        urls::INVITE_PUBKEY.to_string(),
         urls::NAME.to_string(),
+        urls::EMAIL.to_string(),
       ].into(),
       description: "Allows new users to easily, in one request, make both an Agent and a Drive. This drive will be created at the subdomain of `name`.".to_string(),
       shortname: "register".to_string(),
@@ -23,49 +26,125 @@ pub fn register_endpoint() -> Endpoint {
   }
 }
 
-#[tracing::instrument(skip(store))]
-pub fn construct_register_redirect(
-    url: url::Url,
-    store: &impl Storelike,
-    for_agent: Option<&str>,
-) -> AtomicResult<Resource> {
-    let requested_subject = url.to_string();
-    let mut pub_key = None;
+pub fn confirm_email_endpoint() -> Endpoint {
+    Endpoint {
+        path: "/confirmEmail".to_string(),
+        params: [urls::TOKEN.to_string(), urls::INVITE_PUBKEY.to_string()].into(),
+        description: "Confirm email address and set a key for your Agent.".to_string(),
+        shortname: "confirm-email".to_string(),
+        handle: Some(construct_confirm_email_redirect),
+        handle_post: None,
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MailConfirmation {
+    pub email: EmailAddress,
+    pub name: String,
+}
+
+#[tracing::instrument()]
+pub fn construct_register_redirect(context: HandleGetContext) -> AtomicResult<Resource> {
     let mut name_option = None;
-    for (k, v) in url.query_pairs() {
+    let mut email_option: Option<EmailAddress> = None;
+    let store = context.store;
+    for (k, v) in context.subject.query_pairs() {
         match k.as_ref() {
-            "public-key" | urls::INVITE_PUBKEY => pub_key = Some(v.to_string()),
             "name" | urls::NAME => name_option = Some(v.to_string()),
+            "email" => email_option = Some(EmailAddress::new(v.to_string())?),
             _ => {}
         }
     }
-    if pub_key.is_none() && name_option.is_none() {
+    // by default just return the Endpoint
+    if name_option.is_none() && email_option.is_none() {
         return register_endpoint().to_resource(store);
-    }
-
-    let name = if let Some(n) = name_option {
-        n
-    } else {
-        return Err("No name provided".into());
     };
 
-    let drive_creator_agent: String = if let Some(key) = pub_key {
-        let mut new = Agent::new_from_public_key(store, &key)?;
-        new.name = Some(name.clone());
+    let name = name_option.ok_or("No name provided")?;
+    let email = email_option.ok_or("No email provided")?.check_used(store)?;
+
+    // send the user an e-mail to confirm sign up
+    let store_clone = store.clone();
+    let confirmation_token_struct = MailConfirmation {
+        email: email.clone(),
+        name: name.clone(),
+    };
+    let token = crate::token::sign_claim(store, confirmation_token_struct)?;
+    let mut confirm_url = store
+        .get_server_url()
+        .clone()
+        .set_path("confirmEmail")
+        .url();
+    confirm_url.set_query(Some(&format!("token={}", token)));
+    let message = MailMessage {
+        to: email,
+        subject: "Confirm your e-mail address".to_string(),
+        body: format!("Welcome to Atomic Data, {}. Please confirm your e-mail address by clicking the link below", name),
+        action: Some(MailAction {
+            name: "Confirm e-mail address".to_string(),
+            url: confirm_url.into()
+        })
+    };
+    // async, because mails are slow
+    tokio::spawn(async move {
+        store_clone
+            .send_email(message)
+            .await
+            .unwrap_or_else(|e| tracing::error!("Error sending email: {}", e));
+    });
+
+    // Here we probably want to return some sort of SuccesMessage page.
+    // Not sure what that should be.
+    let mut resource = Resource::new_generate_subject(store);
+    resource.set_string(urls::DESCRIPTION.into(), "success", store)?;
+
+    // resource.set_propval(urls::, value, store)
+
+    Ok(resource)
+}
+
+#[tracing::instrument()]
+pub fn construct_confirm_email_redirect(context: HandleGetContext) -> AtomicResult<Resource> {
+    let url = context.subject;
+    let store = context.store;
+    let mut token_opt: Option<String> = None;
+    let mut pubkey_option = None;
+
+    for (k, v) in url.query_pairs() {
+        match k.as_ref() {
+            "token" | urls::TOKEN => token_opt = Some(v.to_string()),
+            "public-key" | urls::INVITE_PUBKEY => pubkey_option = Some(v.to_string()),
+            _ => {}
+        }
+    }
+    let token = if let Some(t) = token_opt {
+        t
+    } else {
+        return confirm_email_endpoint().to_resource(store);
+    };
+    let pubkey = pubkey_option.ok_or("No public-key provided")?;
+
+    // Parse and verify the JWT token
+    let confirmation = crate::token::verify_claim::<MailConfirmation>(store, &token)?.custom;
+
+    // Create the Agent if it doesn't exist yet.
+    // Note: this happens before the drive is saved, which checks if the name is available.
+    // We get new agents that just do nothing, but perhaps that's not a problem.
+    let drive_creator_agent: String = {
+        let mut new = Agent::new_from_public_key(store, &pubkey)?;
+        new.name = Some(confirmation.name.clone());
         let net_agent_subject = new.subject.to_string();
         new.to_resource()?.save(store)?;
         net_agent_subject
-    } else if let Some(agent) = for_agent {
-        if agent == PUBLIC_AGENT {
-            return Err("No `public-key` provided.".into());
-        }
-        agent.to_string()
-    } else {
-        return Err("No `public-key` provided".into());
     };
 
     // Create the new Drive
-    let drive = crate::populate::create_drive(store, Some(&name), &drive_creator_agent, false)?;
+    let drive = crate::populate::create_drive(
+        store,
+        Some(&confirmation.name),
+        &drive_creator_agent,
+        false,
+    )?;
 
     // Add the drive to the Agent's list of drives
     let mut agent = store.get_resource(&drive_creator_agent)?;
@@ -73,6 +152,12 @@ pub fn construct_register_redirect(
         urls::DRIVES,
         SubResource::Subject(drive.get_subject().into()),
         true,
+    )?;
+    // TODO: Make sure this only works if the server sets the email address.
+    agent.set(
+        urls::EMAIL.into(),
+        Value::String(confirmation.email.to_string()),
+        store,
     )?;
     agent.save_locally(store)?;
 
@@ -84,7 +169,5 @@ pub fn construct_register_redirect(
         crate::Value::AtomicUrl(drive_creator_agent),
         store,
     )?;
-    // The front-end requires the @id to be the same as requested
-    redirect.set_subject(requested_subject);
     Ok(redirect)
 }