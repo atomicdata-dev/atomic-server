44ce85856db018685e63174c22b4e145ca172db0
Roel <roelleijser@gmail.com>
2024-10-21 13:15:49 +0200

    Add webpack extension alias for .js extentions in Next.js config

--- browser/create-template/templates/nextjs-site/next.config.mjs
+++ browser/create-template/templates/nextjs-site/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: config => {
+    config.resolve.extensionAlias = {
+      '.js': ['.ts', '.tsx', '.js'],
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
